9d24c1eaee2cb10e77c70c9d9f2aa5037da9b7e2
Andy Thorne <contrabandvr@gmail.com>
2022-01-10 14:11:31 +0000

    Updated phpcsfixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,13 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
+$finder = (new PhpCsFixer\Finder())
     ->exclude('vendor')
     ->exclude('tests/Functional/var')
     ->in('src')
     ->in('tests/Unit')
     ->in('tests/Functional/Doctrine');
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@PSR2' => true,
@@ -16,11 +16,10 @@ return PhpCsFixer\Config::create()
         'ordered_class_elements' => true,
         'phpdoc_order' => true,
         'phpdoc_separation' => true,
-        'general_phpdoc_annotation_remove' => ['author', 'package'],
+        'general_phpdoc_annotation_remove' => ['annotations ' => ['author', 'package']],
         'yoda_style' => [
             'equal' => false,
             'identical' => false,
         ],
     ])
     ->setFinder($finder);
-;
